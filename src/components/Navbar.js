efaf669a789311bacab5533104a2db382fac2d4c
Jorge <alejandro131093@gmail.com>
2021-02-01 21:08:35 -0600

    Header y listas
    JSX: Usando json y <li/>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React,{Component}from 'react';
 
-class Navbar extends React.Component{
+class Navbar extends Component{
     
     render(){
+        let NavbarItems = {
+            Home: "Home",
+            Products: ["Software","IoT"],
+            AboutUs: "About Us",
+        }
         return(
-            <h1>Componente Navbar</h1>
+            <div className = "Navbar">
+                <h1>Componente Navbar</h1>
+                <h2>Componente Navbar items</h2>
+                
+                <hr/>
+                <ol>
+                    {
+                    NavbarItems.Products.map((product, i)=>{
+                        console.log(product);
+                        return( 
+                            <li key = {i}>
+                                
+                                {product}
+                            </li>
+                        )
+                    })
+                    
+
+                     }
+                </ol>
+
+                <h3>Systems</h3>
+            </div>
         );
     }
 }
