64ec9f15fefe86f3b921fd65adcd6f4ec4b4d0d7
Jorge <alejandro131093@gmail.com>
2021-02-01 20:02:14 -0600

    Navbar
    Inicio del navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
+import Navbar from './components/Navbar'
+
+
+function holamundo(name) {
+  var presentacion =  <div>
+    <h2>Hola mi nombre es: </h2>
+    <h3>{name}</h3>
+    </div> ;
+  return presentacion
+}
 
 function App() {
+  var nombre = "Jorge de Paz";
+  
   return (
     <div className="App">
+      <section className = "componentes">
+          <Navbar/>
+      </section>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           En construcción
         </p>
+        {holamundo(nombre)}
       </header>
     </div>
+    
   );
 }
 
